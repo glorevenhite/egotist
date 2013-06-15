83d8bc0ddc814e20ee7f0be1219dff5b23f3f0c6
Vinh Vo <vinh.votruong@gmail.com>
2013-06-15 20:09:18 +0700

    passing data as view variables

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,10 +4,10 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$data = array();
-		$data['site_name'] = 'Egotist';
-		$data['random'] = rand(1,10);
-		$this->response->body(View::factory('welcome', $data));		
+		$view = View::factory('welcome');		
+		$view->site_name = 'Egotist';
+		$view->random = rand(1,10);
+		$this->response->body($view);		
 	}
 
 } // End Welcome
